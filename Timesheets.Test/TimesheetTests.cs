54338e03ee21d1c55186565a87a725260a8eb79e
DESKTOP-RCIVMR8\norbstar <richard.norbron@gmail.com>
2023-10-03 23:32:03 +0100

    Updated the unit test class with additional test cases. These cover the 3 primary functions of the interface:
    - Adding timesheets
    - Sorting timesheet as part of a summary report
    - Exporting timesheets as a CSV file via download

--- Timesheets.Test/TimesheetTests.cs
+++ Timesheets.Test/TimesheetTests.cs
@@ -1,9 +1,5 @@
-using Castle.Core.Configuration;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
 using Moq;
-using System;
-using Timesheets.Infrastructure;
+
 using Timesheets.Models;
 using Timesheets.Repositories;
 using Timesheets.Services;
@@ -12,32 +8,150 @@ namespace Timesheets.Test
 {
     public class TimesheetTests
     {
-        [Fact]
-        public void GivenAValidTimesheet_ThenAddTimesheetToInMemoryDatabase()
+        private Timesheet BuildTimesheet(float hours = 7.5f)
         {
-            //Arrange
-            var timesheet = new Timesheet();
-            var timesheetEntry = new TimesheetEntry()
+            return new Timesheet
             {
-                Id = 1,
-                Date = "01/09/2023",
-                Project = "Test Project",
-                FirstName = "Test",
-                LastName = "Test",
-                Hours = "7.5"
+                Id = 0,
+                TimesheetEntry = new TimesheetEntry
+                {
+                    Id = 0,
+                    Date = "01/09/2023",
+                    Project = "Test Project",
+                    FirstName = "Test",
+                    LastName = "Test",
+                    Hours = hours.ToString()
+                },
+                TotalHours = hours.ToString()
             };
-            timesheet.Id = 1;
-            timesheet.TimesheetEntry = timesheetEntry;
-            timesheet.TotalHours = timesheetEntry.Hours;
+        }
 
+        private TimesheetService GetService {
+            get => new TimesheetService(new TimesheetRepository(new Infrastructure.DataContext(null)));
+        }
+
+        [Fact]
+        public void GivenAValidTimesheet_VerifyAddTransactionWasPerformed()
+        {
+            // Arrange
+            var timesheet = BuildTimesheet();
             var mockRepository = new Mock<ITimesheetRepository>();
+            mockRepository.Setup(repo => repo.AddTimesheet(It.IsAny<Timesheet>()));
+
             var timesheetService = new TimesheetService(mockRepository.Object);
 
             // Act
             timesheetService.Add(timesheet);
 
             // Assert
-            mockRepository.Verify(repo => repo.AddTimesheet(It.IsAny<Timesheet>()), Times.Once);
+            mockRepository.Verify(repo => repo.AddTimesheet(timesheet), Times.Once);
+        }
+
+        [Fact]
+        public void GivenAValidTimesheet_VerifyTimesheetWasStored()
+        {
+            // Arrange
+            var timesheet = BuildTimesheet();
+            var timesheetService = GetService;
+
+            // Act
+            timesheetService.Add(timesheet);
+
+            // Assert
+            Equals(timesheetService.GetAll().Count, 1);
+        }
+
+        [Fact]
+        public void GivenAValidTimesheet_VerifyTimesheetStoredProperties()
+        {
+            // Arrange
+            var timesheet = BuildTimesheet();
+            var timesheetService = GetService;
+
+            // Act
+            timesheetService.Add(timesheet);
+            var timesheetEntry = timesheet.TimesheetEntry;
+
+            // Assert
+            var dbTimesheeteEntry = timesheetService.GetAll()[0].TimesheetEntry;
+            
+            Equals(dbTimesheeteEntry.Date.Equals(timesheetEntry.Date) &&
+                dbTimesheeteEntry.FirstName.Equals(timesheetEntry.FirstName) &&
+                dbTimesheeteEntry.LastName.Equals(timesheetEntry.LastName) &&
+                dbTimesheeteEntry.Project.Equals(timesheetEntry.Project) &&
+                dbTimesheeteEntry.Hours.Equals(timesheetEntry.Hours),
+                true);
+        }
+
+        [Fact]
+        public void GivenAValidTimesheet_VerifyTimesheetIdAsNonZeroPostSubmission()
+        {
+            // Arrange
+            var timesheet = BuildTimesheet();
+            var timesheetService = GetService;
+
+            // Act
+            timesheetService.Add(timesheet);
+
+            // Assert
+            Equals(timesheetService.GetAll()[0].Id != 0, true);
+        }
+
+        [Fact]
+        public void GivenAValidTimesheet_VerifyTimesheetIdPostSubmission()
+        {
+            // Arrange
+            var timesheet = BuildTimesheet();
+            var timesheetService = GetService;
+
+            // Act
+            timesheetService.Add(timesheet);
+
+            // Assert
+            Equals(timesheetService.GetAll()[0].Id, 1);
+        }
+
+        [Fact]
+        public void GivenASetOfValidTimesheets_VerifyTimesheetSummaryDescendingOrdering()
+        {
+            // Arrange
+            var timesheetA = BuildTimesheet(9f);
+            var timesheetB = BuildTimesheet(12f);
+            var timesheetC = BuildTimesheet(2.5f);
+            var timesheetService = GetService;
+
+            // Act
+            timesheetService.Add(timesheetA);
+            timesheetService.Add(timesheetB);
+            timesheetService.Add(timesheetC);
+
+            // Assert
+            int lastHours = int.MaxValue;
+            bool isDescendng = true;
+
+            foreach(var dbTimesheet in timesheetService.SortTimesheetsByHoursDescending(timesheetService.GetAll()))
+            {
+                if (float.Parse(dbTimesheet.TimesheetEntry.Hours) > lastHours)
+                {
+                    isDescendng = false;
+                }
+            }
+
+            Equals(isDescendng, true);
+        }
+
+        [Fact]
+        public void GivenAValidTimesheet_VerifyGeneratedCSVLineCountAgainstDb()
+        {
+            var timesheet = BuildTimesheet();
+            var timesheetService = GetService;
+
+            // Act
+            timesheetService.Add(timesheet);
+
+            // Assert
+            var lineCount = timesheetService.FormatTimesheetDataAsCSV().Split('\n').Length;
+            Equals(lineCount - 1, timesheetService.GetAll().Count);
         }
     }
 }
